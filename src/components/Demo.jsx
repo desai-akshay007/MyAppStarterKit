b017684dc07b11547328280949447daae5f30964
Akshay Desai <adesai0@walmartlabs.com>
2022-11-14 15:28:44 -0800

    Basic Initial Code

---
+++ src/components/Demo.jsx
@@ -0,0 +1,8 @@
+import {useState} from 'react';
+export const Demo = () =>{
+  const [todos,setTodos] = useState({});
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => setTodos(json))
+return <pre> userID: {JSON.stringify(todos,null,4)}</pre>  
+}
