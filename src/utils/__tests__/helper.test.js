6180c06f3921eed27c18a1881e8759391d9f131f
Akshay Desai <adesai0@walmartlabs.com>
2022-11-14 17:29:11 -0800

    added test

--- src/utils/__tests__/helper.test.js
+++ src/utils/__tests__/helper.test.js
@@ -1,6 +1,7 @@
-
+import {findMyColor} from '../helper'
 describe("Filter function", () => {
     test("it should filter by a search term (link)", () => {
-      // actual test
+      const {color,bgColor} = findMyColor(21);
+      expect(color.h).toEqual(21);
     });
   });
