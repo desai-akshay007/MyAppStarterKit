af42d0bade8e200f323b76d0f891b7c466ea46d1
Akshay Desai <adesai0@walmartlabs.com>
2022-11-14 16:39:42 -0800

    New files added for testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
+    transformIgnorePatterns: [
+        "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"
+      ]
   };
-  
+
   module.exports = config;
